a00f2fb2fe6784ccfebc3a020deaa1320ca0bcdb
Nicholas Perkins <nicholas.s.perkins@gmail.com>
2016-02-25 23:40:40 -0600

    basic code for processing unicode text

---
+++ web/blacklist.js
@@ -0,0 +1,60 @@
+var blacklist = {
+    //control keys
+    "\u0000": "NUL",
+    "\u0001": "SOH",
+    "\u0002": "STX",
+    "\u0003": "ETX",
+    "\u0004": "EOT",
+    "\u0005": "ENQ",
+    "\u0006": "ACK",
+    "\u0007": "BEL",
+    "\u0008": "BS",
+    //"\u0009": "HT", tab OK
+    //"\u000A": "LF", \n OK
+    "\u000B": "VT",
+    "\u000C": "FF",
+    "\u000D": "CR",
+    "\u000E": "SO",
+    "\u000F": "SI",
+    "\u0010": "DLE",
+    "\u0011": "DC1",
+    "\u0012": "DC2",
+    "\u0013": "DC3",
+    "\u0014": "DC4",
+    "\u0015": "NAK",
+    "\u0016": "SYN",
+    "\u0017": "ETB",
+    "\u0018": "CAN",
+    "\u0019": "EM",
+    "\u001A": "SIB",
+    "\u001B": "ESC",
+    "\u001C": "FS",
+    "\u001D": "GS",
+    "\u001E": "RS",
+    "\u001F": "US",
+    //del
+    "\u007F": "DEL",
+    //special whitespace
+    "\u00A0": "NO-BREAK SPACE",
+    "\u1680": "OGHAM SPACE",
+    "\u180E": "MONGOLIAN VOWEL SEPARATOR",
+    "\u2000": "EN QUAD",
+    "\u2001": "EM QUAD",
+    "\u2002": "EN SPACE",
+    "\u2003": "EM SPACE",
+    "\u2004": "THREE-PER-EM SPACE",
+    "\u2005": "FOUR-PER-EM SPACE",
+    "\u2006": "SIX-PER-EM SPACE",
+    "\u2007": "FIGURE SPACE",
+    "\u2008": "PUNCTUATION SPACE",
+    "\u2009": "THIN SPACE",
+    "\u200A": "HAIR SPACE",
+    "\u200B": "ZERO WIDTH SPACE",
+    "\u202F": "NARROW NO-BREAK SPACE",
+    "\u205F": "MEDIUM MATHEMATICAL SPACE",
+    "\u3000": "IDEOGRAPHIC SPACE",
+    "\uFEFF": "ZERO WIDTH NO-BREAK SPACE"
+};
+
+
+
--- web/uSpotter.js
+++ web/uSpotter.js
@@ -1,9 +1,34 @@
 (function(){
+    var pasteText = document.getElementById('pasteText');
+    var display = document.getElementById('display');
+    var processButton = document.getElementById('processButton');
 
+    processButton.addEventListener('click',calcDisplay);
 
+    function calcDisplay(){
+        var text = pasteText.value;
+        var length = text.length;
 
+        var output = '';
+        var i = 0;
+        for(;i < length; ++i){
+            var char = text.charAt(i);
+            var code = text.codePointAt(i);
 
+            //normal ASCII range
+            if( code >= 0x0020  && code < 0x007F){
+                 output+= char;
 
+            //bad item
+            }else if (blacklist.hasOwnProperty(char)){
+                output+= '<span style="color:red;background-color:aliceblue">'+blacklist[char]+'</span>';
+            //warning, unknown unicode
+            }else{
+                output+= '<span style="color:orange;background-color:aliceblue">'+code.toString(16)+'</span>';
+            }
+            display.innerHTML = output;
+        }
 
+    }
 
 })();
